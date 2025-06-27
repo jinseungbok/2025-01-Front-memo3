9743eb0df3e8a433bd83528b8fb6bb30945b4e53
jinseungbok <92.bokjs@gmail.com>
2025-06-27 18:12:48 +0900

    [docs}: 250627_delete complete

---
+++ src/components/MemoCard.vue
@@ -0,0 +1,50 @@
+<script setup>
+import httpService from "@/services/HttpService";
+
+const props = defineProps({
+  item: Object,
+});
+
+// 삭제버튼 누르면 콘솔에 id값 출력
+const remove = async () => {
+  console.log("id:", props.item.id);
+  if (confirm("삭제하시겠습니까?")) {
+    console.log("삭제되었습니다.");
+    const params = { memo_id: props.item.id };
+    const data = httpService.deleteMemo(params);
+  }
+};
+</script>
+
+<template>
+  <router-link :to="`/memos/${props.item.id}`" class="item">
+    <div class="d-flex pt-3">
+      <div class="pb-3 mb-0 w-100">
+        <div class="d-flex justify-content-between">
+          <b>{{ props.item.title }}</b>
+          <div>
+            <span role="button" @click.stop.prevent="remove(props.item.id)"
+              >삭제</span
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </router-link>
+</template>
+
+<style lang="scss" scoped>
+.item {
+  background-color: #f8f9fa;
+  border: 1px solid #eee;
+  display: block;
+  color: #000;
+  text-decoration: none;
+  padding: 20px 30px;
+  margin: 15px 0;
+
+  &:hover {
+    border-color: #aaa;
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,56 @@
+<script setup>
+import { reactive, onMounted } from "vue";
+import httpService from "@/services/HttpService";
+import MemoCard from "@/components/MemoCard.vue";
+
+const state = reactive({
+  memos: [],
+});
+
+onMounted(() => {
+  getItems();
+});
+
+const getItems = async (param) => {
+  const data = await httpService.getMemoList(param);
+  state.memos = data.resultData;
+  console.log("state.memos :", state.memos);
+};
+</script>
+
+<template>
+  <div class="memo-list">
+    <MemoCard v-for="m in state.memos" :item="m" :key="m.id" />
+    <!-- v-for 사용시 :key="" 값 반드시 들어가야 함 -->
+
+    <router-link to="/memos/add" class="add btn btn-light">
+      + 추가하기
+    </router-link>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+/*
+.memo-list {
+  .item {
+    background-color: #f8f9fa;
+    border: 1px solid #eee;
+    display: block;
+    color: #000;
+    text-decoration: none;
+    padding: 20px 30px;
+    margin: 15px 0;
+
+    &:hover {
+      border-color: #aaa;
+    }
+  }
+}
+*/
+
+.add {
+  display: block;
+  padding: 25px;
+  border: 1px solid #eee;
+}
+</style>
